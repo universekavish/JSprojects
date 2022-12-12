af6fb99b2610b5e3f3dd9958e0862ff30ad93561
Kavish Boyal <38506513+universekavish@users.noreply.github.com>
2022-12-12 22:12:05 +0530

    Quiz APP

---
+++ quiz-app/script.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+        question : 'How old is Kavish?',
+        a : '10',
+        b : '20',
+        c : '30',
+        d : '23',
+        correct : 'd'
+    },
+    {
+        question : 'What is his favorite language?',
+        a : 'Python',
+        b : 'Java',
+        c : 'JavaScript',
+        d : 'C',
+        correct : 'a'
+    },
+    {
+        question : 'What is his pet\'s name?',
+        a : 'Champ',
+        b : 'Damp',
+        c : 'Lamp',
+        d : 'Camp',
+        correct : 'a'
+    },
+    {
+        question : 'What is he learning currently?',
+        a : 'MERN',
+        b : 'C',
+        c : 'C++',
+        d : 'Nothing',
+        correct : 'a'
+    }
+]
